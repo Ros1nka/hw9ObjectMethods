adfe49fe2fc36b9c8f21996fcf2c659286359e5b
Евгений Страхов <goodwin.s@mail.ru>
2024-07-29 20:07:50 +0300

    hw

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
 
         Author hemingway = new Author("Ernest", "Hemingway");
         Author dostoevsky = new Author("Fyodor", "Dostoevsky");
+        Author dostoevsky1 = new Author("Fyodor", "Dostoevsky");
 
         Book theOldManAndTheSea = new Book("The Old Man and the Sea", hemingway, 1952);
 
@@ -12,6 +13,14 @@ public class App {
 
         System.out.println(theOldManAndTheSea.getName() + ", " + theOldManAndTheSea.getAuthor() + ", " + theOldManAndTheSea.getYear());
         System.out.println(theIdiot.getName() + ", " + theIdiot.getAuthor() + ", " + theIdiot.getYear());
+        System.out.println();
+        System.out.println("Hash: " + hemingway.hashCode());
+        System.out.println("Hash: " + dostoevsky.hashCode());
+        System.out.println("Hash: " + dostoevsky1.hashCode());
+        System.out.println("Equals: " + dostoevsky.equals(hemingway));
+        System.out.println("Equals: " + dostoevsky.equals(dostoevsky1));
+        System.out.println();
+        System.out.println(theIdiot);
         System.out.println();
     }
 }
